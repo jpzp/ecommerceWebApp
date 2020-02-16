ac59ae03a12cc03d73348e70cecd842e9946931d
jpzp <43419134+jpzp@users.noreply.github.com>
2020-02-16 12:27:33 -0500

    Update UserController.java

--- jpzp/src/main/java/com/application/jpzp/controller/UserController.java
+++ jpzp/src/main/java/com/application/jpzp/controller/UserController.java
@@ -1,14 +1,9 @@
 package com.application.jpzp.controller;
 
-import com.application.jpzp.controller.OrderController.OrderForm;
-import com.application.jpzp.dto.OrderProductDto;
+
+
 import com.application.jpzp.exception.ResourceNotFoundException;
-import com.application.jpzp.model.Order;
-import com.application.jpzp.model.OrderProduct;
-import com.application.jpzp.model.OrderStatus;
-import com.application.jpzp.model.Product;
 import com.application.jpzp.model.User;
-import com.application.jpzp.service.ProductService;
 import com.application.jpzp.service.UserService;
 
 import org.springframework.http.HttpHeaders;
@@ -44,61 +39,19 @@ public class UserController {
         return userService.getAllUsers();
     }
     
-    /*
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody OrderForm form) {
-        List<OrderProductDto> formDtos = form.getUser();
-        validateProductsExistence(formDtos);
-        User user = new User();
+    public ResponseEntity<User> create(@RequestBody User user) {
         user = this.userService.create(user);
-
-        List<OrderProduct> orderProducts = new ArrayList<>();
-        for (OrderProductDto dto : formDtos) {
-            orderProducts.add(orderProductService.create(new OrderProduct(order, productService.getProduct(dto
-              .getProduct()
-              .getId()), dto.getQuantity())));
-        }
-
-        order.setOrderProducts(orderProducts);
-
-        this.orderService.update(order);
 
         String uri = ServletUriComponentsBuilder
           .fromCurrentServletMapping()
-          .path("/orders/{id}")
-          .buildAndExpand(order.getId())
+          .path("/users/{id}")
+          .buildAndExpand(user.getId())
           .toString();
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", uri);
 
-        return new ResponseEntity<>(order, headers, HttpStatus.CREATED);
+        return new ResponseEntity<>(user, headers, HttpStatus.CREATED);
     }
-
-    private void validateProductsExistence(List<OrderProductDto> orderProducts) {
-        List<OrderProductDto> list = orderProducts
-          .stream()
-          .filter(op -> Objects.isNull(productService.getProduct(op
-            .getProduct()
-            .getId())))
-          .collect(Collectors.toList());
-
-        if (!CollectionUtils.isEmpty(list)) {
-            new ResourceNotFoundException("Product not found");
-        }
-    }
-
-    public static class UserForm {
-
-        private List<UserDto> userOrders;
-
-        public List<UserDto> getUsers() {
-            return users;
-        }
-
-        public void setProductOrders(List<OrderProductDto> productOrders) {
-            this.productOrders = productOrders;
-        }
-    }
-    */
     
 }
