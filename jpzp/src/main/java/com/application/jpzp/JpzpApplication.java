b8c8222b2677390493e13d0aca9f146e57d50cc4
jpzp <43419134+jpzp@users.noreply.github.com>
2020-02-16 20:44:41 -0500

    Added images to products and one default user

--- jpzp/src/main/java/com/application/jpzp/JpzpApplication.java
+++ jpzp/src/main/java/com/application/jpzp/JpzpApplication.java
@@ -1,7 +1,10 @@
 package com.application.jpzp;
 
 import com.application.jpzp.model.Product;
+import com.application.jpzp.model.User;
+
 import com.application.jpzp.service.ProductService;
+import com.application.jpzp.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,13 +20,21 @@ public class JpzpApplication {
 	@Bean
 	CommandLineRunner runner(ProductService productService) {
 		return args -> {
-			productService.save(new Product(1L, "TV Set", 300.00, "http://placehold.it/200x100"));
-            productService.save(new Product(2L, "Game Console", 200.00, "http://placehold.it/200x100"));
-            productService.save(new Product(3L, "Sofa", 100.00, "http://placehold.it/200x100"));
-            productService.save(new Product(4L, "Icecream", 5.00, "http://placehold.it/200x100"));
-            productService.save(new Product(5L, "Beer", 3.00, "http://placehold.it/200x100"));
-            productService.save(new Product(6L, "iPhone", 500.00, "http://placehold.it/200x100"));
-            productService.save(new Product(7L, "Watch", 30.00, "http://placehold.it/200x100"));
+			productService.save(new Product(1L, "TV Set", 500.00, "https://image.shutterstock.com/image-photo/4k-monitor-isolated-on-white-260nw-357968483.jpg"));
+            productService.save(new Product(2L, "Switch", 300.00, "https://image.shutterstock.com/image-vector/video-game-console-gamepad-vector-260nw-1137618860.jpg"));
+            productService.save(new Product(3L, "Monitor", 200.00, "https://image.shutterstock.com/image-photo/blank-computer-monitor-desk-clipping-260nw-98048864.jpg"));
+            productService.save(new Product(4L, "Laptop", 350.00, "https://image.shutterstock.com/image-photo/laptop-blank-screen-on-table-260nw-340152863.jpg"));
+            productService.save(new Product(5L, "Sunglasses", 50.00, "https://image.shutterstock.com/image-vector/realistic-vector-sunglasses-on-yellow-260nw-662770798.jpg"));
+            productService.save(new Product(6L, "iPhone", 800.00, "https://image.shutterstock.com/image-vector/smartphone-iphone-style-gold-color-260nw-1541350247.jpg"));
+            productService.save(new Product(7L, "Watch", 90.00, "https://image.shutterstock.com/image-photo/luxury-watch-isolated-on-white-260nw-1233640588.jpg"));
 		};
 	}
+	
+	@Bean
+	CommandLineRunner runner2(UserService userService) {
+		return args -> {
+            userService.save(new User(1L, "Juan", "Zambrano", "10/25/1995", "10358 NW 46 st", "Doral", "Fl", "3058771557"));
+		};
+	}
+	
 }
